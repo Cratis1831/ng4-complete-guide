234397b8e998cdd0720b928b31c7d3b84738d61b
asotoudeh18 <asotoudeh18@hotmail.com>
2020-10-27 16:27:18 -0400

    Databind Recipe/Shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe','This is simply a test','https://i.ytimg.com/vi/qCO4o_Aglfw/maxresdefault.jpg')
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://i.ytimg.com/vi/qCO4o_Aglfw/maxresdefault.jpg'
+    ),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is simply a test 2',
+      'https://i.ytimg.com/vi/qCO4o_Aglfw/maxresdefault.jpg'
+    ),
   ];
 
-  constructor() {
-  }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
